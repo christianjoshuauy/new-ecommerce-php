bc0d5370f56244f1bcb6a6c7b214ffc45b7c9071
Kenjie <kbalona5@gmail.com>
2023-05-15 09:00:33 +0800

    Fix redundancy

--- dashboard.php
+++ dashboard.php
@@ -80,17 +80,6 @@ if (!isset($_SESSION['auth']) || !$_SESSION['auth']) {
     </h2>
     <div class="products">
       <?php
-      $servername = "localhost";
-      $username = "root";
-      $password = "";
-      $dbname = "dbecommerce";
-
-      $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-      if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
-
       $sql = "SELECT * FROM product";
       $result = mysqli_query($conn, $sql);
 
