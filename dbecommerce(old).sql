9e92acee1315e58cd648d5ee32b0ae75c485e12c
LAB12_USER ON WS09 <L12X09W09@citulab.net>
2023-05-17 10:20:05 +0800

    Exported new database

--- dbecommerce(old).sql
+++ dbecommerce(old).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2023 at 04:18 AM
+-- Generation Time: May 14, 2023 at 05:47 PM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,18 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `dbecommerce`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
-
-CREATE TABLE `category` (
-  `categoryID` bigint(20) UNSIGNED NOT NULL,
-  `categoryName` varchar(50) DEFAULT NULL,
-  `categoryDescription` varchar(300) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -55,6 +43,7 @@ INSERT INTO `product` (`productID`, `productName`, `productDescription`, `produc
 (1, 'NMD_V3', 'New Arrival Shoes', 'https://assets.adidas.com/images/w_766,h_766,f_auto,q_auto,fl_lossy,c_fill,g_auto/a246dea70d46428493bfaf4800067f1a_9366/nmd_v3-shoes.jpg', 5950),
 (2, 'DAZY Dress', 'Puff Sleeve Zip Back', 'https://img.ltwebstatic.com/images3_pi/2022/12/12/167083594053b1ca6ca4e31656c44a2baa61694040_thumbnail_600x.webp', 596),
 (3, 'Nike SB', 'Woven Skate Long-Sleeve Button Down', 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/737228a2-0013-4772-a63f-1b8c09291253/sb-woven-skate-long-sleeve-button-down-VV6rM1.png', 3895),
+(4, 'LeBron', 'Hip Pack', 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/faa0bf81-191e-4546-9071-336d5ac00278/lebron-hip-pack-s6HLj8.png', 2295),
 (5, 'Utility Backpack', 'Training Backpack 27L', 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-d236a750-a36c-408b-8f35-c36a4d53738b/utility-speed-training-backpack-mTh0wd.png', 3295),
 (6, 'Dri-FIT AeroBill Legacy91', 'Camo Training Cap', 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/2c59851e-8190-4603-8961-271705da1cbd/dri-fit-aerobill-legacy91-camo-training-cap-rc1zZQ.png', 1295),
 (7, 'Forum Low Shoes', 'Men Originals', 'https://assets.adidas.com/images/w_766,h_766,f_auto,q_auto,fl_lossy,c_fill,g_auto/ca6eb69651d94083889cacb60018d8bb_9366/forum-low-shoes.jpg', 5000);
@@ -87,12 +76,6 @@ INSERT INTO `tbluseraccount` (`userid`, `username`, `password`, `firstname`, `la
 --
 
 --
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`categoryID`);
-
---
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
@@ -107,12 +90,6 @@ ALTER TABLE `tbluseraccount`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `categoryID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `product`
